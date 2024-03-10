9a44fb9b2a61e043c675ba8532fcf7cc84e69e71
Camila Castillo-Vilcahuaman <castillo.vilcahuaman@gmail.com>
2024-03-09 23:42:27 -0500

    Add project

---
+++ evaluacion_3/src/main/java/com/example/demo/Evaluacion3Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evaluacion3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evaluacion3Application.class, args);
+	}
+
+}
